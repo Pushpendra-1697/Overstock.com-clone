86224ea5eefd277ded85256b750719ee8c15e707
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-07-22 19:54:24 +0530

    added new files

---
+++ product.js
@@ -0,0 +1,84 @@
+let productData=[
+    {image:"https://ak1.ostkcdn.com/images/products/17813700/Layla-Outdoor-Wicker-Hanging-Basket-Chair-with-Cushions-by-Christopher-Knight-Home-e3568e3c-a41b-405d-b04c-a7b1174d9e78_600.jpg?imwidth=3840",productID:1, price:"76.04",catagory:"Twin XL",para:"Hilford Cantilever 4-piece Heavy Duty Fillable Patio Umbrella Weighted Base Stand",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/0a97164046a601de3001ed229efbf46f537c9c69/Weller-10-Ft.-Offset-Cantilever-Hanging-Patio-Umbrella.jpg?imwidth=3840",productID:2,price:"126.39" ,catagory:"Twin XL",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/9722cf736820de69de4e225b95f06f501613e157/Outsunny-3-Person-Patio-Swing-Chair-Bench-Hammock-Outdoor-with-Convertible-Canopy%2C-Cushion%2C-Pillows-for-Porch-Backyard-Garden.jpg?imwidth=3840",productID:3,price:"152.34",catagory:"Twin XL",para:"Corvus Vera 10-foot Offset Cantilever Hanging Canopy Outdoor Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/ed2d9cb64b791e9c701b5422792cc629acb455c8/Kylie-Outdoor-Wicker-Hanging-Basket-Chair-by-Christopher-Knight-Home.jpg?imwidth=3840",productID:4,price:"86.04",catagory:"Twin XL",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},{image:"https://ak1.ostkcdn.com/images/products/is/images/direct/f91b1f0d552924a7cd0d82fd3477293c82df2ce4/Corvus-Vera-10-foot-Offset-Cantilever-Hanging-Canopy-Outdoor-Patio-Umbrella.jpg?imwidth=3840",productID:5,price:"760.04",catagory:"Twin XL",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},{image:"https://ak1.ostkcdn.com/images/products/is/images/direct/74a6cce26cc2744ea14486280aa04e48268a2441/Cayuse-Teardrop-Hang-Chair-%28Stand-Not-Included%29.jpg?imwidth=3840",productID:6,price:"7600.04",catagory:"Full",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/fc129d338b242d527247398b1abf1fec8b05930c/Pro-Heavy-Duty-Patio-Umbrella-Cross-Brace-Stand.jpg?imwidth=256",productID:7,price:"768.04",catagory:"Full",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/f654ea83fa9833ed3cf67a2616d76c31a3d9b78e/Outsunny-Outdoor-Patio-Porch-Swing-Bench-with-Included-Adjustable-Shade-Awning-%26-Comfort-Padded-Seating-for-Three-People.jpg?imwidth=256",productID:8,price:"786.04",catagory:"Full",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/22c600305a5278f6ed2d7bf092b359ebbe5ea6da/Outsunny-3-Person-Porch-Patio-Swing-with-Adjustable-Weather-Fighting-Tilt-Canopy-%26-Comfortable-Bench-Style-Seat.jpg?imwidth=256",productID:9,price:"769.04",catagory:"Full",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/22d0d72d9de2088f001864503ae9dfe2b93fc8f6/Signature-Design-by-Ashley-Chime-12-inch-Hybrid-Mattress.jpg?impolicy=mediumlow",productID:10,price:"3999.04",catagory:"Full",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/c73db7ca0d9f9669f1605f20a8971d0addb5cea4/VIGO-Elan-E-class-68-in.-x-76-in.-Frameless-Sliding-Shower-Door-in-Matte-Brushed-Gold-with-Clear-Glass.jpg?impolicy=mediumlow",productID:11,price:"7690.04",catagory:"Queen",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/22c600305a5278f6ed2d7bf092b359ebbe5ea6da/Outsunny-3-Person-Porch-Patio-Swing-with-Adjustable-Weather-Fighting-Tilt-Canopy-%26-Comfortable-Bench-Style-Seat.jpg?imwidth=https://ak1.ostkcdn.com/images/products/is/images/direct/e9d67a371c986795f8d9045cd2a9d7a12dfd4287/COSIEST-3-Piece-Oversize-Bistro-Set-Patio-Rocking-Chairs-with-Coffee-Table.jpg?impolicy=mediumlow",productID:12,price:"790.04",catagory:"Queen",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/035e152b4d15401298dabf242ff7ed819aee303a/Brookside-Tara-Square-Tufted-Upholstered-Platform-Bed.jpg?impolicy=mediumlow",productID:13,price:"888.04",catagory:"Queen",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/17040773/Priage-by-Zinus-14-inch-Platforma-Bed-Frame-9efaaefd-5fe5-4b12-a151-71219d064a1e_320.jpg?impolicy=mediumlow",productID:14,price:"7689.04",catagory:"Queen",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+
+  {image:"https://ak1.ostkcdn.com/images/products/17040773/Priage-by-Zinus-14-inch-Platforma-Bed-Frame-9efaaefd-5fe5-4b12-a151-71219d064a1e_320.jpg?impolicy=mediumlow",productID:15,price:"7969.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/a610927bb594f893e9a8409100a90b76c6d3f8f7/Max-%26-Lily-Twin-High-Loft-Bed-with-Bookcase-and-Desk.jpg?impolicy=mediumlow",productID:16,price:"70069.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/22d0d72d9de2088f001864503ae9dfe2b93fc8f6/Signature-Design-by-Ashley-Chime-12-inch-Hybrid-Mattress.jpg?impolicy=mediumlow",productID:17,price:"72269.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/7020875e5270c9b0dffe7fefacf361aba5044fcf/Central-Park-Arsenal-Black-and-White-Gallery-Wrapped-Canvas.jpg?impolicy=mediumlow",productID:18,price:"73369.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/6a6d636a73d9ffb77a25d4b64a06b032473158f0/Slumber-Solutions-14-inch-Gel-Memory-Foam-Choose-Your-Comfort-Mattress.jpg?impolicy=mediumlow",productID:19,price:"72269.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+
+  {image:"https://ak1.ostkcdn.com/images/products/10696097/LUCID-Comfort-Collection-Premium-Hypoallergenic-Waterproof-Mattress-Protector-fff8bed0-4a0d-442b-99b3-db366524450e_320.jpg?impolicy=mediumlow",productID:20,price:"74469.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/d3a359f58e478f65071b62b143ea6be0433ed899/Slumber-Solutions-Active-14-inch-Charcoal-Memory-Foam-Mattress.jpg?impolicy=mediumlow",productID:21,price:"75569.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/e7ae52856f2a028d4740f1557fd677ded2f7915f/Better-Trends-Country-Braid-Reversible-Area-Rug.jpg?impolicy=mediumlow",productID:22,price:"76669.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"},
+
+  {image:"https://ak1.ostkcdn.com/images/products/is/images/direct/19cba60aa27d65a2624939beb166e4d328b88640/LUCID-Comfort-Collection-12-inch-Gel-and-Aloe-Memory-Foam-Mattress.jpg?impolicy=mediumlow",productID:23,price:"78769.04",catagory:"King",para:"Weller 10 Ft. Offset Cantilever Hanging Patio Umbrella",star:"https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"}
+];
+    document.querySelector("#filter").addEventListener("change",handlefilterCategory);
+    function handlefilterCategory(){
+    let selected=document.querySelector("#filter").value;
+    if(selected===""){
+        return displayTable(productData);
+    }else{
+        let filterList=productData.filter(function(ele,index){
+            return ele.catagory===selected;
+        });
+        displayTable(filterList);
+    };
+    
+  }
+    displayTable(productData);
+let cartLS=JSON.parse(localStorage.getItem("cart-page"))||[];
+
+function displayTable(productData){
+    document.querySelector("#container").innerHTML=null;
+    productData.map(function(ele,index){
+        let div=document.createElement("div");
+        let image=document.createElement("img");
+        image.setAttribute("src",ele.image);
+        let price=document.createElement("h2");
+        price.innerText=ele.price;
+        let catagory=document.createElement("h5");
+        catagory.innerText=ele.catagory;
+        let para=document.createElement("p");
+        para.innerText=ele.para;
+        let star=document.createElement("img");
+        star.setAttribute("src",ele.star);
+        let cart=document.createElement("button");
+        cart.innerText="Add To Cart";
+        cart.addEventListener("click",function(){
+            AddtoCart(ele,index);
+        });
+        div.append(image,cart,price,catagory,para,star);
+        document.querySelector("#container").append(div);
+    });
+}
+function AddtoCart(ele,index){
+    let flag=true;
+    for(let i=0;i<cartLS.length;i++){
+        if(cartLS[i].productID===ele.productID){
+            alert("Product already in the Cart");
+            flag=false;
+            return;
+        }
+    }
+    if(flag===true){
+        cartLS.push(ele);
+        alert("Product Added Successfully");
+        localStorage.setItem("cart-page",JSON.stringify(cartLS));
+    }       
+}
+
+
